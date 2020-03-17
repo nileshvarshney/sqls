45ed000b330556b608ad65e08d167435e4c1332e
nileshvarshney <nilesh.varshney@yahoo.com>
2020-03-17 00:48:36 -0700

    initial

---
+++ split_a_string.sql
@@ -0,0 +1,13 @@
+-- mysql (split a comma seperated script )
+
+with recursive cte(n) as 
+( 
+	select 1 as n
+	union all
+	select n +1 from cte where n < 5
+)
+select  c.addressline1, 
+        substring_index(substring_index(c.addressline1,',',n),',',-1) as address_values 
+from cte inner join customers c 
+on (cte.n <= (length(c.addressline1) - length(replace(c.addressline1,',','')))+1)
+order by c.addressline1
