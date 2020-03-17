183c32da9672c75f1bf5f98d559c0dba47722c68
nileshvarshney <nilesh.varshney@yahoo.com>
2020-03-17 00:58:01 -0700

    Update split_a_string.sql

--- split_a_string.sql
+++ split_a_string.sql
@@ -10,4 +10,10 @@ select  c.addressline1,
         substring_index(substring_index(c.addressline1,',',n),',',-1) as address_values 
 from cte inner join customers c 
 on (cte.n <= (length(c.addressline1) - length(replace(c.addressline1,',','')))+1)
-order by c.addressline1
+order by c.addressline1;
+					     
+-- postgres
+select regexp_split_to_table('red,green,blue',',');
+					     
+
+					      
